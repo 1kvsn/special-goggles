ad004b7f6dc2ca911d94072fbe50ddfa5088c3db
1kvsn <sasikant@torum.com>
2024-10-04 10:49:17 +0530

    add test case to test only 1 number as argument to add function

--- src/add.js
+++ src/add.js
@@ -1,3 +1,10 @@
 export function add(str) {
   if (str === "") return 0;
+
+  const numbers = str.split(",");
+
+  return numbers.reduce(
+    (acc, currentVal) => Number(acc) + Number(currentVal),
+    0
+  );
 }
--- src/add.test.js
+++ src/add.test.js
@@ -4,4 +4,9 @@ describe("String Calculator: add function", () => {
   test("should return 0 for an empty string", () => {
     expect(add("")).toBe(0);
   });
+
+
+  test("should return the number if only 1 number is passed as argument", () => {
+    expect(add("1")).toBe(1);
+  });
 });
